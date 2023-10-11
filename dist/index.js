a1decdfd7e088feacd879b87a025d88049eebec4
DenisZaviriukha <dv.zavirukha@gmail.com>
2023-10-11 12:12:56 +0300

    Initial commit

---
+++ dist/index.js
@@ -0,0 +1,94 @@
+// function concatenation(firstWord: string, secondWord: string) {
+//   console.log(`${firstWord} ${secondWord}`);
+// }
+// const button = document.querySelector('button')!;
+// const input = document.querySelector('input')!;
+// if (button && input) {
+//   button.addEventListener('click', () => {
+//     concatenation(input.value, 'hello!');
+//   });
+// }
+// let   total:    number    = 100;
+// const userName: string    = "Bob";
+// const isActive: boolean   = true;
+// const empty:    null      = null
+// const undf:     undefined = undefined
+// const numbers: number[] = [1, 2, 3, 4] // ТИПИЗАЦИЯ МАССИВА
+// numbers.push()
+// const user: {
+//     name: string,
+//     age: number,
+// } = {
+//     name: 'bob',
+//     age: 12,
+// }
+// const ticket = {
+//     filmName: "Joker",
+//     cost: "15$",
+//     timeOfSession: "19:00"
+// }
+// type Ticket = {
+//     filmName: string,
+//     cost: string,
+//     timeOfSession: string,
+// }
+// const ticket: Ticket = {
+//     filmName: "Joker",
+//     cost: "15$",
+//     timeOfSession: "19:00"
+// }
+// type eventType = 'lesson' | 'deadline'
+// const eventik: eventType = 'deadline'
+// type pusi = 'wet' | 'dry' | 'cum '
+// let age: any = 'a'
+// age = true
+// let boobs: unknown ="boob"
+// type size = 'small' | 'medium' | 'large'
+// enum Sizes {
+//     small = "small",
+//     medium = "medium",
+//     large = "large",
+// }
+// const button: size = "small"
+// const button2: Sizes = Sizes.small
+// function plus(num1: number, num2: number): void {
+//     let calc = num1 + num2
+//     console.log(calc)
+//     // return calc.toString()
+// }
+// const plus = (num1: number, num2: number): void => {
+//     console.log(num1 + num2)
+// }
+// type FIOreturn = {
+//     name: string,
+//     surmane: string,
+//     middleName: string,
+// }
+// function FIO (name: string, surname: string, middleName: string): FIOreturn {
+//     return {name, surmane, middleName}
+// }
+// type Car = {
+//     number: number,
+//     color: string,
+//     start: (number: number) => number
+// }
+// const car = {
+//     number: 123,
+//     color: 'red',
+//     start: (number) => {
+//         console.log(number)
+//         return number
+//     }
+// }
+// type WhiteList = {
+//     jacket: boolean,
+//     hat?: boolean
+// }
+// const Carl: WhiteList = {
+//     jacket: true
+// }
+// const Klara: WhiteList = {
+//     jacket: false,
+//     hat: true,
+// }
+//# sourceMappingURL=index.js.map
